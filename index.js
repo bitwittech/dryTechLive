808e1b784add3815a58a46a7532c7872a6ca8ab3
YASH1730 <yashwantsahu3002@gmail.com>
2024-11-13 13:23:46 +0530

    add :: sitemap setting added

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const { sendEmail } = require('./mail/sendMail');
 const PORT = process.env.PORT || 8080;
 const cors = require('cors')
+var cron = require('node-cron');
+const GenerateSitemap = require('./utils/SiteMap');
+const sitemap_update = "0 0 * * *" // At 12:00 AM, every day
+
+cron.schedule(sitemap_update, ()=>updateSiteMap());
 
 app.use(cors());
 
@@ -16,6 +21,16 @@ app.use(bodyParser.json()); // To parse JSON request bodies
 
 app.post("/sendMail",sendEmail)
 
+// Serve the sitemap
+app.get('/sitemap.xml.gz', (req, res) => {
+    console.log('sitemap_update')
+    const filePath = path.join(__dirname, '/utils/public', 'sitemap.xml.gz');
+    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader('Content-Encoding', 'gzip');
+    res.sendFile(filePath);
+  });
+
+
 // Handle any request and serve 'index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -24,3 +39,11 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log('Server is running on port:', PORT);
 });
+
+
+  // Schedule sitemap generation every day at midnight
+function updateSiteMap () {
+    GenerateSitemap()
+      .then(() => console.log('Sitemap generated successfully'))
+      .catch(console.error);
+  };
